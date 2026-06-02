dcc3a0fa65ffb8f223fd8ce146ad8391b07b6f8b
Syed Abdullah <syedmuhammadabdullah35@gmail.com>
2026-06-02 22:05:44 +0500

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,27 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 // 1. Keys
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB_kDZNl24bNHRy89whrnFyTmdpk96jZDY",
   authDomain: "mirhashop.firebaseapp.com",
   projectId: "mirhashop",
-  storageBucket: "mirhashop.appspot.com",
+  storageBucket: "mirhashop.firebasestorage.app",
   messagingSenderId: "869855032923",
-  appId: "1:869855032923:web:4e2a4d126850e6ef5b317e"
+  appId: "1:869855032923:web:4e2a4d126850e6ef5b317e",
+  measurementId: "G-RVV6G7FD16"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // 2. Initialize app
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
