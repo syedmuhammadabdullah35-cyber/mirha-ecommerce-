6b1d4bdc9cbd57a5b1a4a8096f9ea488ad653828
Syed Abdullah <syedmuhammadabdullah35@gmail.com>
2026-06-02 22:50:16 +0500

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,17 +2,8 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// 1. Keys
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_kDZNl24bNHRy89whrnFyTmdpk96jZDY",
+  apiKey: "AIzaSyB_kDZN124bNHRy89whrnFyTmdpk96jZDY",
   authDomain: "mirhashop.firebaseapp.com",
   projectId: "mirhashop",
   storageBucket: "mirhashop.firebasestorage.app",
@@ -22,11 +13,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-// 2. Initialize app
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
-export const db = getFirestore(app); 
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
